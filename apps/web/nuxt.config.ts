21e5a047ac30fbd5bb6a66942f88e52f1206d187
KUN1007 <yuyuyukunkunkun@gmail.com>
2026-03-14 13:18:44 +0800

    feat: adding tailwindcss

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,23 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image']
+
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image'],
+
+  devServer: {
+    host: '127.0.0.1',
+    port: 9420,
+  },
+
+  // Frontend
+  css: ['~/styles/index.css'],
+
+  vite: {
+    // @ts-expect-error
+    plugins: [tailwindcss()],
+  },
 })
