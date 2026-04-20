2a8fc294192202fa80c0a83fb119d71a45629195
KUN1007 <yuyuyukunkunkun@gmail.com>
2026-04-20 11:22:27 +0800

    refactor: update kun alert component and add useKunAlert composable

--- packages/ui/app/components/kun/alert/Alert.vue
+++ packages/ui/app/components/kun/alert/Alert.vue
@@ -1,37 +1,25 @@
 <script setup lang="ts">
-const { showAlert, alertTitle, alertMsg, isShowCancel } = storeToRefs(
-  useComponentMessageStore()
-)
-
-const handleClose = () => {
-  showAlert.value = false
-  useComponentMessageStore().handleClose()
-}
-
-const handleConfirm = () => {
-  showAlert.value = false
-  useComponentMessageStore().handleConfirm()
-}
+const { state, handleConfirm, handleCancel } = useKunAlertState()
 </script>
 
 <template>
   <KunModal
-    :modal-value="showAlert"
-    @update:modal-value="(value) => (showAlert = value)"
+    :modal-value="state.show"
     class-name="z-2000 fixed"
+    @update:modal-value="(value: boolean) => !value && handleCancel()"
   >
     <div class="max-w-80">
       <div class="space-y-2">
-        <h3 class="text-lg" v-if="alertTitle">{{ alertTitle }}</h3>
-        <p class="text-sm" v-if="alertMsg">{{ alertMsg }}</p>
+        <h3 v-if="state.title" class="text-lg">{{ state.title }}</h3>
+        <p v-if="state.message" class="text-sm">{{ state.message }}</p>
       </div>
 
       <div class="mt-6 flex justify-end gap-2">
         <KunButton
-          v-if="isShowCancel ?? true"
+          v-if="state.showCancel"
           variant="light"
           color="danger"
-          @click="handleClose"
+          @click="handleCancel"
         >
           取消
         </KunButton>
